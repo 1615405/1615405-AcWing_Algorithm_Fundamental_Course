83e8b026ebbf4c9a3a41eafc95550d158943ac12
cherry_77 <125164331+1615405@users.noreply.github.com>
2024-07-24 19:54:20 +0800

    Update AcWing_0794.py

--- 第一讲 基础算法/AcWing_0794.py
+++ 第一讲 基础算法/AcWing_0794.py
@@ -1,20 +1,24 @@
 def big_number_division(A, B):
     BASE = 10**9
     split_number = lambda num: [int(num[max(i-9, 0):i]) for i in range(len(num), 0, -9)][::-1]
+    
     a_parts = split_number(A)
     b = int(B)
     remainder = 0
     result = []
+    
     for part in a_parts:
         current = remainder * BASE + part
         result.append(current // b)
         remainder = current % b
+        
     if result:
         final_result = [str(result[0])]
         final_result.extend(f"{num:09d}" for num in result[1:])
         quotient = ''.join(final_result).lstrip('0') or '0'
     else:
         quotient = '0'
+        
     return quotient, remainder
 
 
