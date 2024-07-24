db3b5909c018b5218f3e4c065ce3a38df3612032
cherry_77 <125164331+1615405@users.noreply.github.com>
2024-07-24 16:50:01 +0800

    Update AcWing_0790.py

--- 第一讲 基础算法/AcWing_0790.py
+++ 第一讲 基础算法/AcWing_0790.py
@@ -1,22 +1,17 @@
-def main():
-    x = float(input())
-    
-    if x == 0:
-        print(f"{x:.6f}")
-        return
-    
+def newton_root(x: float) -> float:
+    if x == 0: return 0
     C = x
     x0 = x
     EPS = 1e-8
-
     while True:
         xi = (2 * x0 * x0 * x0 + C) / (3 * x0 * x0)
         if abs(xi - x0) < EPS:
             break
         x0 = xi
-    
-    print(f"{x0:.6f}")
+    return x0
 
 
 if __name__ == "__main__":
-    main()
+    x = float(input())
+    ans = newton_root(x)
+    print(f"{ans:.6f}")
